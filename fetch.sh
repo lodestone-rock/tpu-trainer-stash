8b158e38a37a781a1796c97711f121e90336b08e
kavorite <kavorite@gmail.com>
2023-04-16 06:33:45 -0400

    correct mountpoint syntax; halt on error

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,5 @@
+set -e
 [[ -e posts.csv ]] || curl -OL https://huggingface.co/lodestones/SD-1.5-Test/resolve/main/posts.csv
 [[ -e e6_dump ]] || mkdir e6_dump
-[[ mountpoint -q e6_dump ]] || sudo mount -t tmpfs e6_dump
+[[ $(mountpoint -q e6_dump) ]] || sudo mount -t tmpfs -o size=200G tmpfs e6_dump
 xargs -n 1 curl -L < files.txt | tar xzf - -C e6_dump/
