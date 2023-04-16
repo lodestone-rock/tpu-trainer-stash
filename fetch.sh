0f8b39b7c636db97e8221e952a3e3fbbcb953967
kavorite <kavorite@gmail.com>
2023-04-16 05:04:30 -0400

    file fetching script

---
+++ fetch.sh
@@ -0,0 +1,4 @@
+[[ -e posts.csv ]] || curl -OL https://huggingface.co/lodestones/SD-1.5-Test/resolve/main/posts.csv
+[[ -e e6_dump ]] || mkdir e6_dump
+[[ mountpoint -q e6_dump ]] || sudo mount -t tmpfs e6_dump
+xargs -n 1 curl -L < files.txt | tar xzf - -C e6_dump/
